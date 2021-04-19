3dd37df0771d049de1ed6183b484eb170b349aac
moshiur26 <moshiurrahmanb11@gmail.com>
2021-04-20 00:09:55 +0600

    update MainRouter & yarn add query-string

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -5,10 +5,18 @@ import Users from './user/Users'
 import Signup from './user/Signup'
 import Signin from './auth/Signin'
 import EditProfile from './user/EditProfile'
-import NewShop from './shop/NewShop'
 import Profile from './user/Profile'
 import PrivateRoute from './auth/PrivateRoute'
 import Menu from './core/Menu'
+
+import NewShop from './shop/NewShop'
+import Shops from './shop/Shops'
+import MyShops from './shop/MyShops'
+import Shop from './shop/Shop'
+import EditShop from './shop/EditShop'
+import NewProduct from './product/NewProduct'
+import EditProduct from './product/EditProduct'
+import Product from './product/Product'
 
 const MainRouter = () => {
     return (<div>
@@ -20,7 +28,18 @@ const MainRouter = () => {
         <Route path="/signin" component={Signin}/>
         <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
         <Route path="/user/:userId" component={Profile}/>
+        
+        <Route path="/product/:productId" component={Product}/>
+        <Route path="/shops/all" component={Shops}/>
+        <Route path="/shops/:shopId" component={Shop}/>
+
+        <PrivateRoute path="/seller/shops" component={MyShops}/>
         <PrivateRoute path="/seller/shop/new" component={NewShop}/>
+        <PrivateRoute path="/seller/shop/edit/:shopId" component={EditShop}/>
+        <PrivateRoute path="/seller/:shopId/products/new" component={NewProduct}/>
+        <PrivateRoute path="/seller/:shopId/:productId/edit" component={EditProduct}/>
+
+
       </Switch>
     </div>)
 }
--- client/product/Products.js
+++ client/product/Products.js
@@ -63,9 +63,9 @@ export default function Products(props){
               <GridListTileBar className={classes.tileBar}
                 title={<Link to={"/product/"+product._id} className={classes.tileTitle}>{product.name}</Link>}
                 subtitle={<span>$ {product.price}</span>}
-                actionIcon={
+                // actionIcon={
                 //   <AddToCart item={product}/>
-                }
+                // }
               />
             </GridListTile>
           ))}
